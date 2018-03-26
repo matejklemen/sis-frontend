5dd94cd2ee648c16be7f78991648cfefe4e82966
mc0239 <mc0239@student.uni-lj.si>
2018-03-26 23:10:07 +0200

    Added filter for address format (json object -> nice output)

---
+++ src/app/shared/filters/formatAddress.filter.js
@@ -0,0 +1,34 @@
+(function() {
+/* global angular*/
+
+var formatAddress = function() {
+  return function(addressObject, showCountry) {
+    if(!addressObject) return "/";
+
+    var ab = "";
+    // Append first line of address
+    ab += addressObject.line1;
+    // Append second line of address, if defined
+    if(addressObject.line2.trim().length > 0) {
+        ab += "\n";
+        ab += addressObject.line2;
+    }
+    // Append post number and post title
+    // post.id <=> post number
+    ab += "\n";
+    ab += addressObject.post.id + " " + addressObject.post.name;
+
+    // Append country, if defined
+    if(showCountry && addressObject.country.trim().length > 0) {
+        ab += "\n";
+        ab += addressObject.line1;
+    }
+
+    return ab;
+  };
+};
+
+  angular
+    .module('sis')
+    .filter('formatAddress', formatAddress);
+})();
