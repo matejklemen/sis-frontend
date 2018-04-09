82a4f6d12df659b1f34559d4325ab18cceee6bb5
Blazka Blatnik <bb3172@student.uni-lj.si>
2018-04-09 22:07:40 +0200

    enrolment base form

--- src/app/components/enrol/enrol.ctrl.js
+++ src/app/components/enrol/enrol.ctrl.js
@@ -2,6 +2,7 @@
   var enrolCtrl = function($scope, $routeParams, $window, studentService, tokenService) {
     var vm = this;
 
+    /* Get and set data */
     tokenService.getTokenByStudentId($routeParams.studentId).then(
       function success(response) {
         console.log(response.data);
@@ -22,13 +23,35 @@
         function success(response) {
           console.log(response.data);
           vm.student = response.data;
+          setAndParseStudentData();
         },
         function error(error) {
           
         }
       );
     }
+    
+    function setAndParseStudentData(){
+      vm.parsedStProgram = vm.token.studyProgram.evsCode + " " + (vm.token.studyProgram.name).split('(')[0];
+    }
 
+    /* Helpers */
+    function checkEMSO(emso){
+      numbs = emso.split('');
+      sum = 0;
+      counter = 0;
+      facts=[7,6,5,4,3,2,7,6,5,4,3,2];
+      for(i in numbs){
+        if(counter==12){
+          if(11 - (sum%11) == parseInt(numbs[i])){
+            return true;
+          }
+          return false;
+        }
+        sum += facts[counter] * parseInt(numbs[i]);
+        counter+=1;
+      }
+    }
   };
 
   angular
