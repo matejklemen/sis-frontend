71728a2402020b2e551c67b0943b6aabe5599523
Matej Klemen <mk3141@student.uni-lj.si>
2018-05-17 13:56:42 +0200

    Display warning when trying to delete exam term with sign ups attached to it

--- src/app/shared/directives/examTermsList/deleteExamTermModal.modalctrl.js
+++ src/app/shared/directives/examTermsList/deleteExamTermModal.modalctrl.js
@@ -7,7 +7,16 @@
     vm.userLoginId = resUserLogin;
     vm.userRole = resUserRole;
 
+    vm.deleteWarning = "";
     vm.sendStatus = undefined;
+
+    examTermService.getSignedUpStudentsForExamTerm(vm.examTerm.id).then(
+      function success(response) {
+        vm.deleteWarning = "Opozorilo: za ta izpitni rok že obstajajo prijave - v primeru, da izbrišete izpitni rok, se bodo izbrisale tudi vse prijave na ta izpitni rok.";
+      },
+      function error(error) {
+        console.log("No students are signed up for selected course exam term...");
+      })
 
     vm.deleteExamTerm = function() {
       examTermService.deleteExamTerm(vm.examTerm.id).then(
