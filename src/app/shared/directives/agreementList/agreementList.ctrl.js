c2cf97710724efcb0fa6aa75d57c7e17d3398a69
Matej Klemen <mk3141@student.uni-lj.si>
2018-06-02 00:13:35 +0200

    Added show more/less option to list of agreements

--- src/app/shared/directives/agreementList/agreementList.ctrl.js
+++ src/app/shared/directives/agreementList/agreementList.ctrl.js
@@ -11,6 +11,46 @@
 
     vm.error = {};
 
+    vm.toggleShowSlo = function(idxAgreement) {
+      vm.searchResult[idxAgreement].toggleSlo = !vm.searchResult[idxAgreement].toggleSlo;
+
+      vm.searchResult[idxAgreement].toggleMessageSlo = vm.searchResult[idxAgreement].toggleSlo? "[Manj]": "[...]";
+    }
+
+    vm.toggleShowEng = function(idxAgreement) {
+      vm.searchResult[idxAgreement].toggleEng = !vm.searchResult[idxAgreement].toggleEng;
+
+      vm.searchResult[idxAgreement].toggleMessageEng = vm.searchResult[idxAgreement].toggleEng? "[Less]": "[...]";
+    }
+
+    var trimIfNeeded = function(idxAgreementObj) {
+      vm.searchResult[idxAgreementObj].fullContentSlovene = vm.searchResult[idxAgreementObj].contentSlovene;
+
+      var splitContentSlovene = vm.searchResult[idxAgreementObj].fullContentSlovene.split(" ");
+      var lastWordIdxSlo = splitContentSlovene.length > 10? 10: splitContentSlovene.length;
+
+      // trim after 10th word...
+      vm.searchResult[idxAgreementObj].hasMoreSlo = splitContentSlovene.length > 10;
+
+      if(vm.searchResult[idxAgreementObj].hasMoreSlo)
+        vm.searchResult[idxAgreementObj].toggleMessageSlo = "[...]";
+
+      vm.searchResult[idxAgreementObj].contentSlovene = splitContentSlovene.slice(0, lastWordIdxSlo).join(" ");
+
+      if(vm.searchResult[idxAgreementObj].contentEnglish != null) {
+        vm.searchResult[idxAgreementObj].fullContentEnglish = vm.searchResult[idxAgreementObj].contentEnglish;
+        var splitContentEnglish = vm.searchResult[idxAgreementObj].fullContentEnglish.split(" ");
+        var lastWordIdxEng = splitContentEnglish.length > 10? 10: splitContentEnglish.length;
+
+        vm.searchResult[idxAgreementObj].hasMoreEng = splitContentEnglish.length > 10;
+
+        if(vm.searchResult[idxAgreementObj].hasMoreEng)
+          vm.searchResult[idxAgreementObj].toggleMessageEng = "[...]";
+
+        vm.searchResult[idxAgreementObj].contentEnglish = splitContentEnglish.slice(0, lastWordIdxEng).join(" ");
+      }
+    }
+
     vm.getAgreementList = function() {
       var selectedYr = vm.filterYearOfIssue === undefined ? null: vm.filterYearOfIssue;
 
@@ -18,6 +58,14 @@
         function success(response) {
           vm.totalCount = response.headers('X-total-count');
           vm.searchResult = response.data;
+
+          for(var i = 0; i < vm.searchResult.length; i++) {
+
+            trimIfNeeded(i);
+
+
+          }
+
           console.log(vm.searchResult);
         },
         function error(error) {
